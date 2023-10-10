18bb30bd98aa0f9a993151c7b9250450c500f7bf
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-10-10 08:09:25 -0300

    feat: criado lista em memoria para salvar e listar usuarios

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from 'src/interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
   async createUser(@Body() createUser: CreateUserDto) {
-    return {
-      ...createUser,
-      password: undefined,
-    };
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUser(): Promise<User[]> {
+    return this.userService.getAllUser();
   }
 }
